079d5c6c51e4c48f5373c8a6e365b25c95771ea4
Pratik Bagmare <pratikbagmare@gmail.com>
2023-11-24 00:57:00 -0600

    Integrated Login with backend
    - Integrated Login with backend
    - Conditional Rendering of Header

--- model/signin_model.php
+++ model/signin_model.php
@@ -1,10 +1,37 @@
 <?php
-    // require_once "model/pdo.php";
-    function checkLogin($email,$password,$isAdmin){
-      if($isAdmin){
-        // check in admins table and verify credentials 
+  require_once "model/pdo.php";
+  function checkLogin($email,$password,$isAdmin){
+    $sql = "SELECT * FROM users WHERE email=:email AND isAdmin=:isAdmin;";
+    $stmt = Database::getDB()->prepare($sql);
+    $checkAdmin = $isAdmin ? 'yes' : 'no';
+    $stmt->execute(
+      array(
+        ':email' => $email,
+        ':isAdmin' => $checkAdmin
+      )
+    );
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($row){
+      if(password_verify($password, $row['password'])){
+        return true;
       }else{
-        // check in users/customers table and verify credentials 
+        return false;
       }
+    }else{
+      return false;
     }
+  }
+
+  function getUserDetails($email){
+    $sql = "SELECT * FROM users WHERE email=:email;";
+    $stmt = Database::getDB()->prepare($sql);
+    $stmt->execute(
+      array(
+        ':email' => $email
+      )
+    );
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $row;
+  }
+
 ?>

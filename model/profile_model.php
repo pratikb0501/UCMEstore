b19ae18c72beea8953e1032819a44e081a93a9fa
Pratik Bagmare <pratikbagmare@gmail.com>
2023-11-27 19:35:57 -0600

    integrated update profile
    integrated update profile

--- model/profile_model.php
+++ model/profile_model.php
@@ -1,31 +1,51 @@
 <?php
     require_once "model/pdo.php";
+
     function updateUser($email,$password,$firstName,$lastName,$contactNo,$address,$city,$state,$zipCode,$userID){
-      $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-      $sql = "UPDATE USERS SET email=:email,password=:password,firstname=:firstName,lastName=:lastName,phone=:phone,address=:address,city=:city,state=:state,zipcode=:zipcode,isAdmin=:isAdmin WHERE userID=:userID";
+      $row = checkOtherUserEmail($email);
+      if(!$row){
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "UPDATE USERS SET email=:email,password=:password,firstname=:firstName,lastName=:lastName,phone=:phone,address=:address,city=:city,state=:state,zipcode=:zipcode,isAdmin=:isAdmin WHERE userID=:userID";
+        $stmt = Database::getDB()->prepare($sql);
+        try{
+          $stmt->execute(
+            array(
+              ':email' => $email,
+              ':password' => $hashedPassword,
+              ':firstName' => $firstName,
+              ':lastName' => $lastName,
+              ':phone' => $contactNo,
+              ':address' => $address,
+              ':city' => $city,
+              ':state' => $state,
+              ':zipcode' => $zipCode,
+              ':isAdmin'=> "no",
+              ':userID'=> $userID
+            )
+          );
+          return "Details Updated Sucessfully !";
+        }
+        catch(PDOException $e){
+            print_r($e);
+            return "Failed to Update Details. Please Try Again !";
+        }
+      }else{
+        return "Email ".$email." Already Exists. Please try another email !";
+      }
+    }
+
+    function checkOtherUserEmail($email){
+      $userID = $_SESSION["userID"];
+      $sql = "SELECT * FROM USERS WHERE email=:email AND userID !=:userID";
       $stmt = Database::getDB()->prepare($sql);
-      try{
-        $stmt->execute(
-          array(
-            ':email' => $email,
-            ':password' => $hashedPassword,
-            ':firstName' => $firstName,
-            ':lastName' => $lastName,
-            ':phone' => $contactNo,
-            ':address' => $address,
-            ':city' => $city,
-            ':state' => $state,
-            ':zipcode' => $zipCode,
-            ':isAdmin'=> "no",
-            ':userID'=> $userID
-          )
-        );
-        return "Details Updated Sucessfully !";
-      }
-      catch(PDOException $e){
-          print_r($e);
-          return "Failed to Update Details. Please Try Again !";
-      }
+      $stmt->execute(
+        array(
+          ':email' => $email,
+          ':userID' => $userID
+        )
+      );
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      return $row;
     }
 ?>
 
